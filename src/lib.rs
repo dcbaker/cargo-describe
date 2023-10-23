07b48f5b20bd221c87844a71e17ca0db52e9c79b
Dylan Baker <dylan@pnwbakers.com>
2023-10-23 09:38:55 -0700

    get a basic example working

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 // Copyright © 2023 Dylan Baker
 // SPDX-License-Identifier: MIT
 
-use std::collections::HashMap;
-use std::env;
-use std::vec::Vec;
-use cfg_expr::{Expression, Predicate};
 use cfg_expr::targets::get_builtin_target_by_triple;
-use serde::{Deserialize, Deserializer};
+use cfg_expr::{Expression, Predicate};
+use semver::{Version, VersionReq};
 use serde::de::Error;
-use semver::VersionReq;
+use serde::{Deserialize, Deserializer};
+use std::collections::HashMap;
+use std::vec::Vec;
+use std::{env, fs, path, process};
 
 #[derive(Deserialize, Debug)]
 struct Manifest {
@@ -44,29 +44,65 @@ struct Metadata {
 fn parse(text: &str) -> Vec<(String, VersionReq)> {
     let mani: Manifest = toml::from_str(text).unwrap();
     let target = env::var("TARGET").unwrap();
-    let mut ret: Vec<(String, VersionReq)> =vec![];
+    let mut ret: Vec<(String, VersionReq)> = vec![];
 
-    mani.package.metadata.compiler_versions.iter().for_each( |(k, v)| {
-        match v {
-            Constraint::VersionReq(ver) => ret.push((k.clone(), ver.clone())),
-            Constraint::Cfg(c) => {
-                let cfg = Expression::parse(k).unwrap();
-                let res = if let Some(tinfo) = get_builtin_target_by_triple(&target) {
-                    cfg.eval( |p| match p {
-                        Predicate::Target(tp) => tp.matches(tinfo),
-                        _ => false,
-                    })
-                } else {
-                    false
-                };
-                if res {
-                    c.iter().for_each( |(ck, cv)| { ret.push((ck.clone(), cv.clone())); } );
+    mani.package
+        .metadata
+        .compiler_versions
+        .iter()
+        .for_each(|(k, v)| {
+            match v {
+                Constraint::VersionReq(ver) => ret.push((k.clone(), ver.clone())),
+                Constraint::Cfg(c) => {
+                    let cfg = Expression::parse(k).unwrap();
+                    let res = if let Some(tinfo) = get_builtin_target_by_triple(&target) {
+                        cfg.eval(|p| match p {
+                            Predicate::Target(tp) => tp.matches(tinfo),
+                            _ => false,
+                        })
+                    } else {
+                        false
+                    };
+                    if res {
+                        c.iter().for_each(|(ck, cv)| {
+                            ret.push((ck.clone(), cv.clone()));
+                        });
+                    }
                 }
-            },
-        };
-    });
+            };
+        });
 
     return ret;
+}
+
+fn get_rustc_version() -> String {
+    let rustc = env::var("CARGO_BUILD_RUSTC").unwrap();
+    let out = process::Command::new(rustc)
+        .arg("--version")
+        .output()
+        .expect("Could not run rustc for version");
+
+    String::from_utf8(out.stdout)
+        .expect("Did not get valid output from rustc")
+        .split(" ")
+        .collect::<Vec<&str>>()[1]
+        .to_string()
+}
+
+pub fn evaluate() {
+    let rustc_ver = Version::parse(get_rustc_version().as_str()).unwrap();
+
+    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let p: path::PathBuf = [root, "Cargo.toml".to_string()].iter().collect();
+    let contents = fs::read_to_string(p).unwrap();
+    let checks = parse(&contents);
+
+    checks.iter().for_each(|(name, version)| {
+        if version.matches(&rustc_ver) {
+            println!("cargo:rustc-cfg=compiler_support_{}", name);
+        }
+
+    });
 }
 
 #[cfg(test)]
@@ -77,10 +113,13 @@ mod tests {
 
     #[test]
     fn test_basic_read() {
-        let mani: Manifest = toml::from_str(r#"
+        let mani: Manifest = toml::from_str(
+            r#"
             [package.metadata.compiler_versions]
             foo = "1.0.0"
-        "#).unwrap();
+        "#,
+        )
+        .unwrap();
 
         let v = &mani.package.metadata.compiler_versions["foo"];
         let ver = match v {
@@ -92,10 +131,13 @@ mod tests {
 
     #[test]
     fn test_cfg() {
-        let mani: Manifest = toml::from_str(r#"
+        let mani: Manifest = toml::from_str(
+            r#"
             [package.metadata.compiler_versions.'cfg(target_os = "linux")']
             foo = "1.0.0"
-        "#).unwrap();
+        "#,
+        )
+        .unwrap();
 
         let v = &mani.package.metadata.compiler_versions["cfg(target_os = \"linux\")"];
         let cfg = match v {
@@ -110,14 +152,16 @@ mod tests {
     #[test]
     fn test_parse_cfg() {
         temp_env::with_var("TARGET", Some("x86_64-unknown-linux-gnu"), || {
-            let vals = parse(r#"
+            let vals = parse(
+                r#"
                 [package.metadata.compiler_versions]
                 foo = "1.0.0"
                 [package.metadata.compiler_versions.'cfg(target_os = "linux")']
                 bar = "1.2.0"
                 [package.metadata.compiler_versions.'cfg(target_os = "windows")']
                 bad = "1.2.0"
-            "#);
+            "#,
+            );
 
             assert_eq!(vals.len(), 2);
         });
