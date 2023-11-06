d88cee475b9829245a9968f785d1e09297f239ad
Dylan Baker <dylan@pnwbakers.com>
2023-11-06 14:11:11 -0800

    tests: reduce env patching when not needed
    Some of these were just copy-n-pasted and never needed, some where made
    unneeded by changes to the code.

--- src/cfg.rs
+++ src/cfg.rs
@@ -30,37 +30,34 @@ pub fn check<W: io::Write>(writer: &mut W, checks: &Checks) {
 mod tests {
     use super::*;
     use crate::manifest::parse;
-    use std::{env, fs, path};
-    use temp_env;
+    use std::{fs, path};
 
-    fn get_checks() -> Checks {
-        let root =
-            env::var("CARGO_MANIFEST_DIR").expect("Cargo manifest environment variable unset");
-        let p: path::PathBuf = [root, "Cargo.toml".to_string()].iter().collect();
+    fn get_checks(root: &str) -> Checks {
+        let p: path::PathBuf = [root, "Cargo.toml"].iter().collect();
         let contents = fs::read_to_string(p).expect("Could not read Cargo.toml");
         parse(&contents)
     }
 
     #[test]
     fn test_basic() {
-        temp_env::with_vars(
-            [
-                ("CARGO_MANIFEST_DIR", Some("test_cases/basic")),
-                ("RUSTC", Some("rustc")),
-                ("TARGET", Some("x86_64-unknown-linux-gnu")),
-            ],
-            || {
-                let expected = vec!["cargo:rustc-check-cfg=cfg(foo)", "cargo:rustc-check-cfg=cfg(bar, values(\"a\", \"b\"))"];
+        let expected = vec![
+            "cargo:rustc-check-cfg=cfg(foo)",
+            "cargo:rustc-check-cfg=cfg(bar, values(\"a\", \"b\"))",
+        ];
 
-                let mut out = Vec::new();
-                check(&mut out, &get_checks());
-                let val = std::str::from_utf8(&out).unwrap();
-                let found: Vec<&str> = val.strip_suffix("\n").unwrap().split("\n").collect();
-                assert_eq!(expected.len(), found.len(), "Got wrong number of elements, expected {}, got {}", expected.len(), found.len());
-                for v in found {
-                    assert!(expected.contains(&v), "'{v}' not found in {expected:?}");
-                }
-            },
-        )
+        let mut out = Vec::new();
+        check(&mut out, &get_checks("test_cases/basic"));
+        let val = std::str::from_utf8(&out).unwrap();
+        let found: Vec<&str> = val.strip_suffix("\n").unwrap().split("\n").collect();
+        assert_eq!(
+            expected.len(),
+            found.len(),
+            "Got wrong number of elements, expected {}, got {}",
+            expected.len(),
+            found.len()
+        );
+        for v in found {
+            assert!(expected.contains(&v), "'{v}' not found in {expected:?}");
+        }
     }
 }
--- src/compiler.rs
+++ src/compiler.rs
@@ -11,53 +11,37 @@ pub fn check<W: io::Write>(writer: &mut W, checks: &Checks) {
         if condition.check(&RUSTC) {
             writeln!(writer, "cargo:rustc-cfg={}", name).unwrap();
         }
-    };
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::manifest::parse;
-    use std::{env, fs, path};
+    use std::{fs, path};
     use temp_env;
 
-    fn get_checks() -> Checks {
-        let root =
-            env::var("CARGO_MANIFEST_DIR").expect("Cargo manifest environment variable unset");
-        let p: path::PathBuf = [root, "Cargo.toml".to_string()].iter().collect();
+    fn get_checks(root: &str) -> Checks {
+        let p: path::PathBuf = [root, "Cargo.toml"].iter().collect();
         let contents = fs::read_to_string(p).expect("Could not read Cargo.toml");
         parse(&contents)
     }
 
     #[test]
     fn test_emits() {
-        temp_env::with_vars(
-            [
-                ("CARGO_MANIFEST_DIR", Some("test_cases/basic")),
-                ("CARGO_BUILD_RUSTC", Some("rustc")),
-                ("TARGET", Some("x86_64-unknown-linux-gnu")),
-            ],
-            || {
-                let mut out = Vec::new();
-                check(&mut out, &get_checks());
-                assert_eq!(out, b"cargo:rustc-cfg=foo\n");
-            },
-        )
+        temp_env::with_var("TARGET", Some("x86_64-unknown-linux-gnu"), || {
+            let mut out = Vec::new();
+            check(&mut out, &get_checks("test_cases/basic"));
+            assert_eq!(out, b"cargo:rustc-cfg=foo\n");
+        })
     }
 
     #[test]
     fn test_not_emits() {
-        temp_env::with_vars(
-            [
-                ("CARGO_MANIFEST_DIR", Some("test_cases/not")),
-                ("CARGO_BUILD_RUSTC", Some("rustc")),
-                ("TARGET", Some("x86_64-unknown-linux-gnu")),
-            ],
-            || {
-                let mut out = Vec::new();
-                check(&mut out, &get_checks());
-                assert_eq!(out.len(), 0);
-            },
-        )
+        temp_env::with_var("TARGET", Some("x86_64-unknown-linux-gnu"), || {
+            let mut out = Vec::new();
+            check(&mut out, &get_checks("test_cases/not"));
+            assert_eq!(out.len(), 0);
+        })
     }
 }
